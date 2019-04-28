666dcdabfa4eda6ea0a0e15c3e2707b19c0ec504
188842 <guoyuanping@ncarzone.com>
2019-04-28 09:48:33 +0800

    lliais

--- tools/test.py
+++ tools/test.py
@@ -1,1 +1,10 @@
-print('nihaos')
+import requests
+class test_httprequest:
+    def request_1(self,url):
+        self.url=url
+        res=requests.get(url)
+        return res
+if __name__ == '__main__':
+    url='http://www.baidu.com'
+    res=test_httprequest().request_1(url)
+    print(res.status_code)
